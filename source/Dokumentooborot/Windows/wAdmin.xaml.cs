2da56ea64f23f430dd019a0ae946ee03020b6ecf
Feniks726 <chalmov@yandex.ru>
2022-04-11 13:07:17 +0300

    Filter into wAdmin window

--- source/Dokumentooborot/Windows/wAdmin.xaml.cs
+++ source/Dokumentooborot/Windows/wAdmin.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Input;
@@ -14,6 +15,7 @@ namespace Dokumentooborot.Windows
         BDEntities db;
         public wAdmin()
         {
+            db = new BDEntities();
             InitializeComponent();
         }
 
@@ -59,11 +61,27 @@ namespace Dokumentooborot.Windows
         }
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            db = new BDEntities();
-            dgogrenci.ItemsSource = db.Documents.ToList();
-            
+            LoadTypeDocuments();
+            LoadPlaceStore();
+            chbRelevant.IsChecked = null;
+            Find_LostFocus(sender, e);
         }
 
+
+        private void LoadTypeDocuments()
+        {
+            List<Doc_tipe> typeDocs = db.Doc_tipe.ToList();
+            typeDocs.Insert(0, new Doc_tipe { Id = -1, Name = string.Empty });
+            cmbTypeDocuments.ItemsSource = typeDocs.ToList();
+        }
+
+        private void LoadPlaceStore()
+        {
+            List<Save_place> placeStores = db.Save_place.ToList();
+            placeStores.Insert(0, new Save_place { Id = -1, Name = string.Empty });
+            cmbPlaceStore.ItemsSource = placeStores.ToList();
+        }
+ 
         private void FileClick(object sender, RoutedEventArgs e)
         {
             Document u = this.dgogrenci.SelectedItem as Document;
@@ -75,6 +93,88 @@ namespace Dokumentooborot.Windows
             {
                FileStream fs = File.Open(filename, FileMode.Open);
             }            
+        }
+
+        private void Find_LostFocus(object sender, RoutedEventArgs e)
+        {
+            SearchDocuments();
+        }
+
+        private void txtIndexSearch_LostFocus(object sender, RoutedEventArgs e)
+        {
+            SearchDocuments();
+        }
+
+        private void txtNumberOrder_LostFocus(object sender, RoutedEventArgs e)
+        {
+            SearchDocuments();
+        }
+
+        private void cmbTypeDocuments_LostFocus(object sender, RoutedEventArgs e)
+        {
+            SearchDocuments();
+        }
+
+        private void cmbPlaceStore_LostFocus(object sender, RoutedEventArgs e)
+        {
+            SearchDocuments();
+        }
+
+        private void dateActionTo_LostFocus(object sender, RoutedEventArgs e)
+        {
+            SearchDocuments();
+        }
+
+        private void dateOrder_LostFocus(object sender, RoutedEventArgs e)
+        {
+            SearchDocuments();
+        }
+
+        private void chbRelevant_LostFocus(object sender, RoutedEventArgs e)
+        {
+            SearchDocuments();
+        }
+
+        private void SearchDocuments()
+        {
+            List<Document> documents = db.Documents.ToList();
+
+            if (!string.IsNullOrEmpty(Find.Text))
+            {
+                documents = documents.Where(t => t.Name.Contains(Find.Text)).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(txtIndexSearch.Text))
+            {
+                documents = documents.Where(t => t.Index.Contains(txtIndexSearch.Text)).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(cmbTypeDocuments.Text))
+            {
+                documents = documents.Where(t => t.Doc_tipe_id == ((Doc_tipe)cmbTypeDocuments.SelectedValue).Id).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(cmbPlaceStore.Text))
+            {
+                documents = documents.Where(t => t.Save_place_id == ((Save_place)cmbPlaceStore.SelectedValue).Id).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(dateActionTo.Text))
+            {
+                documents = documents.Where(t => t.Validity_period <= dateActionTo.SelectedDate).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(dateOrder.Text))
+            {
+                documents = documents.Where(t => t.Date_order == dateOrder.SelectedDate).ToList();
+            }
+
+            if (chbRelevant.IsChecked != null)
+            {
+                documents = documents.Where(t => t.Relevance == chbRelevant.IsChecked.Value).ToList();
+            }
+
+            dgogrenci.ItemsSource = documents.ToList();
         }
     }
 }
